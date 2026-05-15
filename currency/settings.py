9c529c7c7689e71513b6280ba213ef4e63e7feec
Rohit Bhatia <bhatiarohit015@gmail.com>
2026-05-15 22:59:22 +0530

    Ready for deployment

--- currency/settings.py
+++ currency/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-me-gq95z%51@qthms$)=d-m_zk56c72l$9)2*%ihxq1htw$+4x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
